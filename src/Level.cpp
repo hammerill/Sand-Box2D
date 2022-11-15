c5b46f0c5e65c88b916ee8a6987f5f22b12f2a23
Hammerill <www.hammerill@gmail.com>
2022-11-15 11:47:28 +0100

    Continue realizing Level JSON reader

--- src/Level.cpp
+++ src/Level.cpp
@@ -1,5 +1,151 @@
 #include "Level.h"
 
-Level::Level(std::string base, std::string filepath) {}
+float Level::getRandomFloat(float min, float max)
+{
+    return  min
+            +
+            static_cast<float> (rand())
+            /
+            (static_cast<float> (RAND_MAX / (max-min)));
+}
 
-void Level::LoadFile(std::string base, std::string filepath) {}
+float Level::LoadNumber(Json::Value input)
+{
+    try
+    {
+        if (input.asString().find_last_of("-") != std::string::npos)
+        {
+            float min = std::stof(input.asString().substr(0, input.asString().find_last_of("-")));
+            float max = std::stof(input.asString().substr(input.asString().find_last_of("-") + 1));
+
+            return Level::getRandomFloat(min, max);
+        }
+        else 
+        {
+            return input.asFloat();
+        }
+    }
+    catch(const std::exception& e)
+    {
+        return NULL;
+    }    
+}
+
+void Level::ProceedPObj(Json::Value jsonObj, std::vector<JsonPObj*>* arrayPObjs)
+{
+    std::string objType = jsonObj["type"].asString();
+
+    // Yes. It's if/else if/else if/else if/...
+    // Yes. It's not a switch.
+    if (objType == "platform")
+    {
+        JsonPObjPlatform* platform;
+        platform->type = objType;
+
+        platform->x1 = Level::LoadNumber(jsonObj["x1"]);
+        platform->y1 = Level::LoadNumber(jsonObj["y1"]);
+        platform->x2 = Level::LoadNumber(jsonObj["x2"]);
+        platform->y2 = Level::LoadNumber(jsonObj["y2"]);
+
+        platform->r = Level::LoadNumber(jsonObj["r"]);
+        platform->g = Level::LoadNumber(jsonObj["g"]);
+        platform->b = Level::LoadNumber(jsonObj["b"]);
+
+        arrayPObjs->push_back(platform);
+    }
+    else if (objType == "box")
+    {
+        JsonPObjBox* box;
+        box->type = objType;
+
+        box->x = Level::LoadNumber(jsonObj["x"]);
+        box->y = Level::LoadNumber(jsonObj["y"]);
+        box->w = Level::LoadNumber(jsonObj["w"]);
+        box->h = Level::LoadNumber(jsonObj["h"]);
+
+        box->angle = Level::LoadNumber(jsonObj["angle"]);
+
+        box->vel_x = Level::LoadNumber(jsonObj["vel_x"]);
+        box->vel_y = Level::LoadNumber(jsonObj["vel_y"]);
+
+        box->texture = jsonObj["texture"].asString();
+    }
+    else if (objType == "circle")
+    {
+        JsonPObjCircle* circle;
+        circle->type = objType;
+
+        circle->x = Level::LoadNumber(jsonObj["x"]);
+        circle->y = Level::LoadNumber(jsonObj["y"]);
+        
+        circle->radius = Level::LoadNumber(jsonObj["radius"]);
+
+        circle->vel_x = Level::LoadNumber(jsonObj["vel_x"]);
+        circle->vel_y = Level::LoadNumber(jsonObj["vel_y"]);
+
+        circle->r = Level::LoadNumber(jsonObj["r"]);
+        circle->g = Level::LoadNumber(jsonObj["g"]);
+        circle->b = Level::LoadNumber(jsonObj["b"]);
+
+        circle->r_angle = Level::LoadNumber(jsonObj["r_angle"]);
+        circle->g_angle = Level::LoadNumber(jsonObj["g_angle"]);
+        circle->b_angle = Level::LoadNumber(jsonObj["b_angle"]);
+    }
+}
+
+Level::~Level()
+{
+    objects->clear();
+    cycles->clear();
+}
+
+std::exception* Level::LoadFile(std::string base, std::string filepath)
+{
+    try
+    {
+        std::ifstream ifs((base + "/" + filepath).c_str());
+
+        Json::Reader reader;
+        Json::Value jsonLevel;
+
+        reader.parse(ifs, jsonLevel);
+
+        // METADATA
+        Level::metadata = jsonLevel["metadata"];
+        ///////////
+
+        // OPTIONS
+        // sorry nothing
+        //////////
+
+        // CAMERA
+        Level::camera.type = jsonLevel["type"].asString();
+
+        if (Level::camera.type != "attached")
+        {
+            Level::camera.x = Level::LoadNumber(jsonLevel["x"]);
+            Level::camera.y = Level::LoadNumber(jsonLevel["y"]);
+            Level::camera.move = jsonLevel["move"].asBool();
+        }
+
+        Level::camera.zoom = jsonLevel["zoom"].asBool();
+        Level::camera.height    = Level::LoadNumber(jsonLevel["height"]);
+        /////////
+
+        // OBJECTS
+        const Json::Value& jsonObjects = jsonLevel["objects"];
+        for (int i = 0; i < jsonObjects.size(); i++)
+        {
+            Level::ProceedPObj(jsonObjects[i], Level::objects);
+        }
+        
+        //////////
+
+        return nullptr;
+    }
+    catch(std::exception* e)
+    {
+        return e;
+    }
+    
+}
--- src/Level.h
+++ src/Level.h
@@ -6,6 +6,18 @@
 #include <fstream>
 #include <vector>
 
+struct JsonOptions
+{
+
+};
+
+struct JsonCamera
+{
+    std::string type;
+    bool zoom, move;
+    float x, y, height;
+};
+
 /// @brief Structure of objects that stored as the array in the Level class
 struct JsonPObj
 {
@@ -30,25 +42,59 @@ struct JsonPObjCircle : JsonPObj
     uint8_t r_angle, g_angle, b_angle;
 };
 
+struct JsonCycle
+{
+    int delay;
+    std::vector<JsonPObj*>* objects = new std::vector<JsonPObj*>();
+};
+
 /// @brief Class that represents Sand-Box2D level loaded from JSON. 
 /// Its fields should be handled by some level loader.
+/// For further info refer to the JSON level file documentation:
+/// https://github.com/Hammerill/Sand-Box2D-levels/
 class Level
 {
 private:
+    float getRandomFloat(float min, float max);
 
+    /// @brief Load number (parse it), further info here:
+    /// https://github.com/Hammerill/Sand-Box2D-levels/blob/main/docs/README-level-file.md#numbers-input
+    /// @param input JSON value to be parsed. It can be number itself or string random ("100-200")
+    /// @return NULL if something went wrong, otherwise number represented as float. 
+    float LoadNumber(Json::Value input);
+
+    /// @brief Array of loaded cycles from JSON represented as structs.
+    /// Should be accessed through getter (see getCycles()).
+    std::vector<JsonCycle*>* cycles = new std::vector<JsonCycle*>();
+
+    /// @brief Parse PObject and add it to array.
+    /// @param jsonObj PObj from JSON to be proceeded.
+    /// @param arrayPObjs array to which add parsed PObj.
+    void ProceedPObj(Json::Value jsonObj, std::vector<JsonPObj*>* arrayPObjs);
 public:
-    /// @brief Create a new Level instance.
-    /// @param base where is directory of all the levels stored on the platform are located?
-    /// @param filepath where is the level file (*.json) located relatively to the "base"?
-    /// Will load this file.
-    Level(std::string base, std::string filepath);
+    ~Level();
+
+    /// @brief Represents "metadata" sector from the file.
+    /// Returned as Json::Value because it can contain custom data.
+    Json::Value metadata;
+
+    /// @brief Represents "options" sector from the file.
+    JsonOptions options;
+
+    /// @brief Represents "camera" sector from the file.
+    JsonCamera camera;
 
     /// @brief Array of loaded objects from JSON represented as structs.
-    std::vector<JsonPObj>* pObjects = new std::vector<JsonPObj>();
+    std::vector<JsonPObj*>* objects = new std::vector<JsonPObj*>();
 
-    /// @brief Load JSON file and represent it as an array of structs (see pObjects).
+    /// @brief Get array of loaded cycles from JSON represented as structs.
+    /// Should be accessed through getter because it should return random values every iteration.
+    std::vector<JsonCycle*>* getCycles();
+
+    /// @brief Load JSON file and represent it as an array of structs (see Level::objects).
     /// @param base where is directory of all the levels stored on the platform are located?
     /// @param filepath where is the level file (*.json) located relatively to the "base"?
     /// Will load this file.
-    void LoadFile(std::string base, std::string filepath);
+    /// @return exception if something went wrong, nullptr if everything's ok.
+    std::exception* LoadFile(std::string base, std::string filepath);
 };
