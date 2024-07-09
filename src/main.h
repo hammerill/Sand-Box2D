304a4ac941f26b52fb3e607ee14e653894aa0f35
hammerill <kyrylo@hammerill.com>
2024-07-09 14:00:57 +0200

    Add experimental macOS support

--- src/main.h
+++ src/main.h
@@ -8,6 +8,9 @@ const char* DEF_SETTINGS_FILE = "./assets/default_settings/windows.json";
 #elif defined(Linux)
 const char* SETTINGS_FILE = "./settings.json";
 const char* DEF_SETTINGS_FILE = "./assets/default_settings/linux.json";
+#elif defined(Macos)
+const char* SETTINGS_FILE = "./settings.json";
+const char* DEF_SETTINGS_FILE = "./assets/default_settings/macos.json";
 #elif defined(Vita)
 const char* SETTINGS_FILE = "ux0:/data/Sand-Box2D/settings.json";
 const char* DEF_SETTINGS_FILE = "./assets/default_settings/vita.json";
